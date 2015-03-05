2baf4080466b1a41ad262cd2bc3c6e51004f5c03
Robin Smedberg <rsmedberg86@gmail.com>
2015-03-05 13:36:37 +0100

    test7

--- app/src/main/java/com/example/sgrn/schoo/Information.java
+++ app/src/main/java/com/example/sgrn/schoo/Information.java
@@ -1,8 +1,6 @@
 package com.example.sgrn.schoo;
 
-/**
- * Created by Fam on 2015-03-04.
- */
+
 public class Information {
     int iconId;
     String title;
--- app/src/main/java/com/example/sgrn/schoo/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/sgrn/schoo/NavigationDrawerFragment.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,7 +28,7 @@ public class NavigationDrawerFragment extends Fragment {
     public static final String KEY_USER_LEARNED_DRAWER = "user_learned_drawer";
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-
+    private RobsAdapter adapter;
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
     private View containerView;
@@ -47,16 +51,31 @@ public class NavigationDrawerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View layout=inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
-        recyclerView= (RecyclerView) layout.findViewById(R.id.drawerList);
+        View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter=new RobsAdapter(getActivity(),getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return layout;
     }
 
+    public static List<Information> getData() {
+        List<Information> data = new ArrayList<>();
+        int[] icons = {R.drawable.ic_action_accept, R.drawable.ic_action_call, R.drawable.ic_action_cancel, R.drawable.ic_action_group};
+        String[] titles = {"Betting", "Standings", "Stats", "Friends"};
+        for (int i = 0; i < titles.length && i < icons.length; i++) {
+            Information current = new Information();
+            current.iconId=icons[i];
+            current.title=titles[i];
+            data.add(current);
+        }
+        return data;
+    }
 
     public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
         containerView = getActivity().findViewById(fragmentId);
         mDrawerLayout = drawerLayout;
-        mDrawerToggle = new ActionBarDrawerToggle(getActivity(),drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);

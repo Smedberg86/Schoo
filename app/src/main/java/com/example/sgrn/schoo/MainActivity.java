222f756f672d73b33c4d77a7259673c89f1b77cf
Robin Smedberg <rsmedberg86@gmail.com>
2015-03-10 13:17:16 +0100

    app2

--- app/src/main/java/com/example/sgrn/schoo/MainActivity.java
+++ app/src/main/java/com/example/sgrn/schoo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.sgrn.schoo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -9,8 +10,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.sgrn.schoo.Tabs.SlidingTabLayout;
 
@@ -34,7 +38,10 @@ public class MainActivity extends ActionBarActivity {
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
         mPager= (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
         mTabs= (SlidingTabLayout) findViewById(R.id.main_activity_tabs);
+        mTabs.setViewPager(mPager);
+
 
 
 
@@ -70,18 +77,40 @@ public class MainActivity extends ActionBarActivity {
 
     class MyPagerAdapter extends FragmentPagerAdapter{
 
+        String[] tabs;
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
+            tabs=getResources().getStringArray(R.array.betting_tabs);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            MyFragment myFragment=MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return 2;
+        }
+    }
+    public static class MyFragment extends Fragment{
+        public static MyFragment getInstance(int position){
+            MyFragment myFragment=new MyFragment();
+            return myFragment;
+        }
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+            View layout=inflater.inflate(R.layout.betting_upcoming, container, false);
+
+            return layout;
         }
     }
 
